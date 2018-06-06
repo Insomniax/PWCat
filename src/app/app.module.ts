936b2215f29ca3d1d66be36396077b4a828e4a1c
Elias Eliades <elias.eliades@foodtecsolutions.com>
2018-06-06 06:35:09 -0400

    Upload project to github

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {MatToolbarModule, MatCardModule} from '@angular/material';
 import { ImgCardComponent } from './img-card/img-card.component';
+import {ServiceWorkerModule} from '@angular/service-worker';
+import {environment} from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -13,7 +15,8 @@ import { ImgCardComponent } from './img-card/img-card.component';
   imports: [
     BrowserModule,
     MatToolbarModule,
-    MatCardModule
+    MatCardModule,
+    environment.production ? ServiceWorkerModule.register('ngsw-worker.js') : []
   ],
   providers: [],
   bootstrap: [AppComponent]
