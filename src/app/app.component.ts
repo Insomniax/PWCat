c75ef10ad75d6b7eddd8160099f09f46af611875
Elias Eliades <elias.eliades@foodtecsolutions.com>
2018-07-23 05:54:53 -0400

    Takis 25

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {SwPush} from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Progressive Web Cat';
+
+  readonly VAPID_PUBLIC_KEY = "BA6kEBuF4ZALTANS2a5KNNPaBsBR5euqzBatZcGRTiW3zgSV7d0kfpNTuOjpzQco62Y0yP6A612TDrhp9DpADVk";
+
+  constructor(private swPush: SwPush) {}
+
+  subscribeToNotifications() {
+    debugger;
+    this.swPush.requestSubscription({
+      serverPublicKey: this.VAPID_PUBLIC_KEY
+    }).then(sub => console.log('SUB', sub))
+      .catch(err => console.error('Could not subscribe to notifications', err));
+  }
 }
